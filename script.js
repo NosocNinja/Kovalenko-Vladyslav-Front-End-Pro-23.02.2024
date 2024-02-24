ce879c502f988193acd782184cd5134068c58fc6
Vlad <vlkovalenk@gmail.com>
2024-02-24 10:04:18 +0100

    second commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let firstNumber = prompt('Give me a first Number');
+    secondNumber = prompt('Give me a second Number');
+    additionNumber = Number(firstNumber) + Number(secondNumber);
+    subtrationNumber = firstNumber - secondNumber; 
+    multiplicationNumber = firstNumber * secondNumber;
+    divisionNumber = firstNumber / secondNumber;
+
+alert('Addition: ' + additionNumber + '; ' + 'Subtration: ' + subtrationNumber + '; ' + 'Multiplication: ' + multiplicationNumber + '; '+ 'Division: ' + divisionNumber + ';');
